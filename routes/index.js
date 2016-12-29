46be64ad67b271baf04552afcb12b43446d50998
Carlos Mendes Aderaldo <cmendesce@gmail.com>
2016-12-29 17:00:04 -0300

    Add ontology view

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 const express = require('express'), 
   router = express.Router()
 
-router.get('/', function(req, res, next) {
-  res.render('index')
+router.get('/', (req, res, next) => res.render('index'))
+router.get('/ontology', (req, res, next) => res.render('ontology'))
+router.get('/runner', (req, res, next) => res.render('runner'))
+
+router.post('/runner', (req, res, next) => {
+  const query = req.body.query
+  const endpoint = req.body.endpoint
+  const client = new SparqlClient(endpoint)
+  client.query(query).execute((error, results) => {
+    res.render('runner', {
+      endpoint: endpoint,
+      query: query,
+      result: results
+    })
+  })
 })
 
 module.exports = router
